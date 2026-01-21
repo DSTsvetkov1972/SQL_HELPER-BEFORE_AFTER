a793c1ec3e79a52b1489dd5ac068f28072987179
Цветков Дмитрий Сергеевич <TsvetkovDS@TRCONT.RU>
2026-01-21 07:23:58 +0300

    add symbols to clean

--- test.py
+++ test.py
@@ -1,6 +1,2 @@
-import os
-
-conditions={}
-with open(os.path.join(os.getcwd(), 'conditions.json'), 'r', encoding="utf-8") as file:  
-    conditions['json'] = file.read()
-    print(conditions['json'])
+s = '�'
+print(ord(s[-1]))
